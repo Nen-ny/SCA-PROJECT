4761cc5a9d1b3d79853d6c7d8fa2f0a6146c2ac3
Nen-ny <105357982+Nen-ny@users.noreply.github.com>
2022-08-18 23:21:45 +0100

    added files

---
+++ Tip-Calculator/script.js
@@ -0,0 +1,121 @@
+const billAmount = document.getElementById('input-bill');
+let tipBtns = document.querySelectorAll('.tip');
+const tipCustom = document.getElementById('input-tip');
+const people = document.getElementById('input-people');
+const errorMsg = document.querySelector('.error-msg');
+const results = document.querySelectorAll('.value');
+const resetBtn = document.querySelector('.reset');
+
+billAmount.addEventListener('input', setBillValue);
+
+tipBtns = Array.from(tipBtns);
+
+console.log(tipBtns);
+
+tipBtns.forEach(btn => {
+    btn.addEventListener('click', handleClick);
+});
+
+tipCustom.addEventListener('input', setTipCustomValue);
+
+people.addEventListener('input', setPeopleValue);
+
+resetBtn.addEventListener('click', reset);
+
+
+let billValue = 0.0;
+let tipValue = 0.15;
+let peopleValue = 1;
+
+function validateFloat(s) {
+    let rgx = /^[0-9]*\.?[0-9]*$/;
+    return s.match(rgx);
+}
+
+function validateInt(s) {
+    let rgx =  /^[0-9]*$/;
+    return s.match(rgx);
+}
+
+function setBillValue() {
+    if (billAmount.value.includes(',')){
+        billAmount.value = billAmount.value.replace(',','.');
+    }
+
+    if (!validateFloat(billAmount.value)) {
+        billAmount.value = billAmount.value.substring(0, billAmount.value,length-1);
+    }
+
+    billValue = parseFloat(billAmount.value);
+
+    calculateTip();
+}
+
+function handleClick(event) {
+    tipBtns.forEach(btn => {
+        btn.classList.remove('btn-active');
+
+        if (event.target.innerHTML == btn.innerHTML){
+            btn.classList.add('btn-active');
+            tipValue = parseFloat(btn.innerHTML)/100;
+        }
+    });
+
+    tipCustom.value = '';
+
+    calculateTip();
+}
+
+function setTipCustomValue() {
+    if(!validateInt(tipCustom.value)) {
+        tipCustom.value = tipCustom.value.substring(0, tipCustom.value.length-1);
+    }
+
+    tipValue = parseFloat(tipCustom.value/100);
+
+    tipBtns.forEach(btn =>{
+        btn.classList.remove('btn-active');
+    });
+
+    if (tipCustom.value !== '') {
+        calculateTip();
+    }
+    
+}
+
+function setPeopleValue(){
+    if (!validateInt(people.value)) {
+        people.value = people.value.substring(0, people.value.length-1);
+    }
+
+    peopleValue = parseFloat(people.value);
+
+    if (peopleValue <= 0) {
+        errorMsg.classList.add('show-error-msg');
+        setTimeout(function(){
+            errorMsg.classList.remove('show-error-msg');
+        }, 5000);
+    }
+
+    calculateTip();
+
+}
+
+function calculateTip() {
+    if (peopleValue >= 1) {
+        let tipAmount = billValue * tipValue / peopleValue;
+        let total = billValue * (tipValue + 1) / peopleValue;
+        results[0].innerHTML = '$' + tipAmount.toFixed(2);
+        results[1].innerHTML = '$' + total.toFixed(2);
+    }
+}
+
+function reset() {
+    billAmount.value = '0.0';
+    setBillValue();
+
+    tipBtns[2].click();
+
+    people.value = '1';
+    setPeopleValue();
+}
